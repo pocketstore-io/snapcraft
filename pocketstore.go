b40f482c79b51fa3885bbc57b297a38382b33bd3
jm@habit-streak.app <support@habit-streak.app>
2025-04-11 22:20:30 +0200

    update pocketstore with real docker compose

--- pocketstore.go
+++ pocketstore.go
@@ -1,9 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("PocketStore running:")
-	fmt.Println("Port 3000 for Nuxt")
-	fmt.Println("Port 8090 for PocketBase")
+	// Define the flags
+	demoFlag := flag.Bool("demo", false, "Clone the demo repository")
+	repoFlag := flag.String("repo", "", "The URL of the repository to clone (required if --demo is not used)")
+
+	// Parse the flags
+	flag.Parse()
+
+	// Determine which repository to clone
+	var repoURL string
+	if *demoFlag {
+		repoURL = "https://github.com/pocketstore-io/demo.git"
+	} else if *repoFlag != "" {
+		repoURL = *repoFlag
+	} else {
+		fmt.Println("Error: You must specify either --demo or --repo=<repository_url>")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	// Clone the repository
+	fmt.Printf("Cloning repository: %s\n", repoURL)
+	cmd := exec.Command("git", "clone", repoURL)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Failed to clone repository: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Repository cloned successfully.")
 }
